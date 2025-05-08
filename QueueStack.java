c9f27e95f3757048f89bd5ae28117b91c1babb55
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-05-08 13:25:11 +0530

    [Siddharth] ADD: Check Palindrome using Queue Stack

---
+++ QueueStack.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class QueueStack {
+    public String checkPalindrome(){
+        Queue<Character> q = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        for(int i=0 ; i<s.length() ;  i++){
+            stack.push(s.charAt(i));
+            q.add(s.charAt(i));
+        }
+
+        for(int i=0 ; i<s.length() ; i++){
+            if(stack.pop() != q.poll()){
+                System.out.print("The word, " + s + ", is not a palindrome.");
+                return;
+            }
+        }
+
+        System.out.print("The word, " + s + ", is a palindrome.");
+
+        sc.close();
+    }
+}
+
