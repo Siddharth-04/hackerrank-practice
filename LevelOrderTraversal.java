fa3b3d7e46694c3aa69c1c0f7327fd37e80db810
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-05-12 12:07:03 +0530

    [Siddharth] ADD: Level Order Traversal

---
+++ LevelOrderTraversal.java
@@ -0,0 +1,31 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+}
+
+public class LevelOrderTraversal{
+    public static void levelOrder(Node root) {
+        if(root == null) return;
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+
+            for(int i=0 ; i<size ; i++){
+                Node front = q.poll();
+                System.out.print(front.data + " ");
+
+                if(front.left != null){
+                    q.add(front.left);
+                }
+
+                if(front.right != null){
+                    q.add(front.right);
+                }
+            }
+        }
+    }
+}
