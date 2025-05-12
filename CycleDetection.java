4301ed1a34d9dc334bf9e51c65db049582962512
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-05-12 12:12:26 +0530

    [Siddharth] ADD: Cycle Detection in Linked list

---
+++ CycleDetection.java
@@ -0,0 +1,19 @@
+class SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode next;
+}
+public class CycleDetection {
+    static boolean hasCycle(SinglyLinkedListNode head) {
+        SinglyLinkedListNode slow = head;
+        SinglyLinkedListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) return true;
+        }
+
+        return false;
+    }
+}
